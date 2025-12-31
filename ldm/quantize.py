fd57eeb519b6045fb1f9bb19b5f9a74bd1ee1d19
drozda <drozda@cerfacs.fr>
2025-12-31 14:40:11 +0100

    Add batch dimension if required

--- ldm/quantize.py
+++ ldm/quantize.py
@@ -274,6 +274,11 @@ class VectorQuantizer2(nn.Module):
         assert temp is None or temp==1.0, "Only for interface compatible with Gumbel"
         assert rescale_logits==False, "Only for interface compatible with Gumbel"
         assert return_logits==False, "Only for interface compatible with Gumbel"
+
+        # Add batch dimension if required
+        if z.ndim == 3:
+            z = z.unsqueeze(0)
+
         # reshape z -> (batch, height, width, channel) and flatten
         z = rearrange(z, 'b c h w -> b h w c').contiguous()
         z_flattened = z.view(-1, self.e_dim)
